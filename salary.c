24812f8da19b01d7ef11d7039defe78043381439
dev-jarir08 <your-email-address>
2024-10-15 16:04:08 +0530

    first program

---
+++ salary.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main() {
+	float bs,hra,da,ta,gross;
+	 bs=100;
+	 hra=(10*bs)/100;
+	 da=(5*bs)/100;
+	 ta=(8*bs)/100;
+	 gross=bs+hra+da+ta;
+	printf("%f",gross);
+}
---
+++ thirdangle.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+main() {
+	int AC=65,BC=45;
+	int AB=180-AC-BC;
+	printf("%d",AB);
+}
